b81b925d0e3fcc5fb1b27513181765c8e17485bb
SantosAndrewz <sentosandrewz@gmail.com>
2024-04-29 02:10:37 -0700

    Display 'Hello HBNB'

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+- A python script that starts a Flask web application.
+- The web application listens on 0.0.0.0, port 5000
+- Route '/' displays "Hello HBNB!".
+
+Usage: 
+    pytho3 -m web_flask.0-hello_route
+    In another tab:  curl 0.0.0.0:5000 ; echo "" | cat -e
+'''
+from flask import Flask
+
+''' Creates a Flask application instance'''
+app = Flask(__name__)
+
+''' Describes the route of the URL'''
+@app.route("/", strict_slashes=False)
+
+def hello_hbnb():
+    '''Function  displays "Hello HBNB!" on running the URL'''
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    
+    ''' Running the  Flask appllication'''
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin//python3
+'''
+A python file to initialize the web_flask package.
+'''
