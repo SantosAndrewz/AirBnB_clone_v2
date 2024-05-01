ae4914fac71f09682cc6a3faa8fddd7d6cf4e45e
SantosAndrewz <sentosandrewz@gmail.com>
2024-04-30 20:56:44 -0700

    Display C + a formatted text

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,8 +5,8 @@
 - Route '/' displays "Hello HBNB!".
 
 Usage:
-    python3 -m web_flask.1-hbnb_route
-    In another tab:  curl 0.0.0.0:5000/hbnb ; echo "" | cat -e
+    python3 -m web_flask.2-c_route
+    In another tab:  curl 0.0.0.0:5000/c/is_fun  ; echo "" | cat -e
 '''
 from flask import Flask
 
@@ -28,7 +28,15 @@ def call_HBNB():
     return "HBNB"
 
 
+@app.route("/c/<text>", strict_slashes=False)
+def call_c_text(text):
+    '''Function displays "C <followed by the text value>"'''
+
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
 if __name__ == "__main__":
 
-    ''' Running the  Flask appllication'''
+    ''' Running the  Flask application'''
     app.run(host='0.0.0.0', port='5000', debug=True)
